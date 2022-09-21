ce82db48bf63b371bd664e22822b196ed957cebc
Baxtigul <jumabekova.baxtigul@gmail.com>
2022-09-21 20:41:42 +0500

    a

--- src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
+++ src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
@@ -1,9 +1,15 @@
 package school.mjc.stage0.loops.task1;
 
 public class RepeatIsBad {
-
+    static int counter = 21;
     @SuppressWarnings("java:S106")
     public void repeatIsBad(){
-
+        counter--;
+        if(counter==0)
+            return;
+        else {
+            System.out.print("writing the same code doesn't have much impact, and it's also time consuming\n");
+            repeatIsBad();
+        }
     }
 }
